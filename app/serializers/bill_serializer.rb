01cabd32f889b646c06d8b25952de94941dfd194
github username <github email address>
2019-07-23 15:43:02 -0400

    added bills table, serializer and model

---
+++ app/serializers/bill_serializer.rb
@@ -0,0 +1,3 @@
+class BillSerializer < ActiveModel::Serializer
+  attributes :id, :congress, :bill_id, :chamber, :number, :title, :short, :title, :sponsor_id, :govtrack_url, :introduced_date, :active, :last_vote, :house_passage, :senate_passage, :enacted, :vetoed, :cosponsors, :cosponsors_by_party, :committees, :primary_subject
+end
---
+++ db/migrate/20190723194053_create_bills.rb
@@ -0,0 +1,25 @@
+class CreateBills < ActiveRecord::Migration[5.2]
+  def change
+    create_table :bills do |t|
+      t.string :congress
+      t.string :bill_id
+      t.string :chamber
+      t.string :number
+      t.string :title
+      t.string :short_title
+      t.string :sponsor_id
+      t.string :govtrack_url
+      t.string :introduced_date
+      t.boolean :active
+      t.string :last_vote
+      t.boolean :house_passage
+      t.boolean :senate_passage
+      t.boolean :enacted
+      t.boolean :vetoed
+      t.integer :cosponsors
+      t.text :cosponsors_by_party
+      t.text :committees
+      t.string :primary_subject
+    end
+  end
+end
