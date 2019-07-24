eb375803c43b8f825b908d77d7212f4356568488
github username <github email address>
2019-07-24 14:50:59 -0400

    search logic back in members controller, everything works

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_23_212352) do
+ActiveRecord::Schema.define(version: 2019_07_24_182058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,11 @@ ActiveRecord::Schema.define(version: 2019_07_23_212352) do
     t.integer "age"
     t.string "twitter_handle"
     t.boolean "running_for_president"
+  end
+
+  create_table "searches", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
