78e15a7ce5da3feb9d02b7e9433831b72350147c
github username <github email address>
2019-07-24 20:07:23 -0400

    can now create bills with full info, associated with members

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -12,12 +12,32 @@ def search
   bills.each do |bill|
     member = Member.find_by(propublica_id: bill["sponsor_id"])
     if !Bill.find_by(bill_id: bill["bill_id"])
-      Bill.create!(short_title: bill["short_title"]
-      binding.pry
+      Bill.create!(
+        bill_id: bill["bill_id"],
+        congress: bill["congress"],
+        bill_type: bill["bill_type"],
+        number: bill["number"],
+        title: bill["title"],
+        short_title: bill["short_title"],
+        sponsor_id: bill["sponsor_id"],
+        govtrack_url: bill["govtrack_url"],
+        introduced_date: bill["introduced_date"],
+        active: bill["active"],
+        last_vote: bill["last_vote"],
+        house_passage: bill["house_passage"],
+        senate_passage: bill["senate_passage"],
+        enacted: bill["enacted"],
+        vetoed: bill["vetoed"],
+        cosponsors: bill["cosponsors"],
+        cosponsors_by_party: bill["cosponsors_by_party"],
+        committees: bill["committees"],
+        primary_subject: bill["primary_subject"],
+        member_id: member.id
+      )
     end
   end
 
-  member = Member.find_by(propublica_id: id)
+  member = Member.find_by(propublica_id: member.id)
   if member.bills
     render json: member.bills
   else
@@ -31,36 +51,8 @@ end
     render json: @bills
   end
 
-  private
-    def bill_params
-      params.require(:bill).permit(:bill_id, :congress, :bill_type, :number, :title, :short_title, :sponsor_id, :gov_track_url, :introduced_date, :active, :last_vote, :house_passage, :senate_passage, :enacted, :vetoed, :cosponsors, :cosponsors_by_party, :committees, :primary_subject, :member_id)
-    end
-
-
-end
   # private
   #   def bill_params
-  #     params.require(:bill).permit(:bill_id, :congress, :bill_type, :number, :title, :short_title, :sponsor_id, :gov_track_url, :introduced_date, :active, :last_vote, :house_passage, :senate_passage, :enacted, :vetoed, :cosponsors, :cosponsors )
-
-  # Bill.create!(
-  #   bill_id: bill["bill_id"],
-  #   congress: bill["congress"],
-  #   bill_type: bill["bill_type"],
-  #   number: bill["number"],
-  #   title: bill["title"],
-  #   short_title: bill["short_title"],
-  #   sponsor_id: bill["sponsor_id"],
-  #   govtrack_url: bill["govtrack_url"],
-  #   introduced_date: bill["introduced_date"],
-  #   active: bill["active"],
-  #   last_vote: bill["last_vote"],
-  #   house_passage: bill["house_passage"],
-  #   senate_passage: bill["senate_passage"],
-  #   enacted: bill["enacted"],
-  #   vetoed: bill["vetoed"],
-  #   cosponsors: bill["cosponsors"],
-  #   cosponsors_by_party: bill["cosponsors_by_party"],
-  #   committees: bill["committees"],
-  #   primary_subject: bill["primary_subject"],
-  #   member_id: member.id
-  # )
+  #     params.require(:bill).permit(:bill_id, :congress, :bill_type, :number, :title, :short_title, :sponsor_id, :gov_track_url, :introduced_date, :active, :last_vote, :house_passage, :senate_passage, :enacted, :vetoed, :cosponsors, :cosponsors_by_party, :committees, :primary_subject, :member_id)
+  #   end
+end
