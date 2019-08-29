141b7ed1c33157476a8b90da3b18a2c340e41d5d
github username <github email address>
2019-08-29 11:12:27 -0400

    added colum searchable_string

--- db/migrate/20190829151209_add_searchable_string_to_members.rb
+++ db/migrate/20190829151209_add_searchable_string_to_members.rb
@@ -1,4 +1,4 @@
-class AddSearchTermColToMember < ActiveRecord::Migration[5.2]
+class AddSearchableStringToMembers < ActiveRecord::Migration[5.2]
   def change
     add_column :members, :searchable_string, :string
   end
