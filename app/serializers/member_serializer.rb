90acda080ca6ee4b2b8eb2239c558e2085c8c3a5
bebopvinh <vinhdo07@gmail.com>
2020-03-24 20:20:46 -0700

    add :search_term, sort attributes alphabetically

--- app/serializers/member_serializer.rb
+++ app/serializers/member_serializer.rb
@@ -4,5 +4,6 @@ class MemberSerializer < ActiveModel::Serializer
   has_many :donors
 
 
-  attributes :id, :propublica_id, :crp_id, :chamber, :title, :short_title, :first_name, :last_name, :full_name, :title_and_name, :date_of_birth, :age, :gender, :searchable_string, :party, :party_full_name, :party_logo, :image_url, :twitter_account, :facebook_account, :youtube_account, :website, :contact_form, :seniority, :next_election, :office, :phone, :state, :state_full_name, :district, :missed_votes_pct, :votes_with_party_pct, :running_for_president, :status, :status_note, :phone_clickable
+  attributes :age, :chamber, :contact_form, :crp_id, :date_of_birth, :district, :facebook_account, :first_name, :full_name, :gender, :id, :image_url, :last_name, :missed_votes_pct, :next_election, :office, :party, :party_full_name, :party_logo, :phone, :phone_clickable, :propublica_id, :running_for_president, :searchable_string, :search_term, :seniority, :short_title, :state, :state_full_name, :status, :status_note, :title, :title_and_name, :twitter_account, :votes_with_party_pct, :website, :youtube_account
+
 end
